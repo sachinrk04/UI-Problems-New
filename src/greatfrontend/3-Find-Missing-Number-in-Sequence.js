3e74be79e14e16a7b3a5a12ed005e324f98859cf
Sachin Singh <sachin.singh@card91.io>
2025-10-06 18:03:26 +0530

    add missing no.

--- src/greatfrontend/3-Find-Missing-Number-in-Sequence.js
+++ src/greatfrontend/3-Find-Missing-Number-in-Sequence.js
@@ -1,3 +1,10 @@
 function findMissingNumberInSequence(numbers) {
-  throw "Not implemented!";
+  const maxNo = Math.max(numbers);
+  const missings = [];
+
+  for (let i = 0; i <= maxNo; i++) {
+    if (!numbers.includes(i)) missings[missings.length] = i;
+  }
+
+  return missings;
 }
