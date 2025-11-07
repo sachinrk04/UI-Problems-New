a022a9ea8274450a0904484f4b6aa1a494b5ef2a
SACHIN SINGH <sachinsingh@Sachins-MacBook-Pro.local>
2025-11-07 10:11:14 +0530

    add new question

--- src/greatfrontend/4-Maximum-Product-in-Contiguous-Array.js
+++ src/greatfrontend/4-Maximum-Product-in-Contiguous-Array.js
@@ -1,3 +1,9 @@
 function maxProductSubArray(numbers) {
   throw "Not implemented!";
 }
+
+console.log(maxProductSubArray([1, 2, -3, 5, 1])); // 5
+console.log(maxProductSubArray([9])); // 9
+console.log(maxProductSubArray([1, 2, 0, -1, 8, -4])); // 32
+console.log(maxProductSubArray([0])); // 0
+console.log(maxProductSubArray([])); // 0
