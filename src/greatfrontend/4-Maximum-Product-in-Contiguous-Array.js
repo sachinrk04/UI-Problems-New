5bb3f5e227a210c7befffa5b01e134101f9ae23f
sachinCARD06 <sachin.singh@card91.io>
2026-01-08 16:56:36 +0530

    add two sum

--- src/greatfrontend/4-Maximum-Product-in-Contiguous-Array.js
+++ src/greatfrontend/4-Maximum-Product-in-Contiguous-Array.js
@@ -1,5 +1,24 @@
 function maxProductSubArray(numbers) {
-  throw "Not implemented!";
+  if (numbers.length === 0) return 0;
+
+  let max = numbers[0];
+  let min = numbers[0];
+  let result = numbers[0];
+
+  for (let i = 1; i < numbers.length; i++) {
+    const current = numbers[i];
+
+    if (current < 0) {
+      [max, min] = [min, max]
+    }
+
+    max = Math.max(current, max * current)
+    min = Math.min(current, min * current)
+
+    result = Math.max(result, max);
+  }
+
+  return result;
 }
 
 console.log(maxProductSubArray([1, 2, -3, 5, 1])); // 5
