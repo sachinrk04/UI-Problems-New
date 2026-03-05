c1518adb1fa286ceae4d0d468e00206dcdc3fee6
sachinCARD06 <sachin.singh@card91.io>
2026-03-05 11:30:30 +0530

    improve sub-sidebar

--- src/components/SubSidbar.tsx
+++ src/components/SubSidbar.tsx
@@ -1,4 +1,5 @@
-import { Link, useLocation } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { NavLink, useLocation } from "react-router-dom";
 
 interface SubSidbarRoute {
   name: string;
@@ -15,6 +16,16 @@ const SubSidbar = ({
   title?: string;
 }) => {
   const location = useLocation();
+  const activeRef = useRef<HTMLAnchorElement | null>(null);
+
+  useEffect(() => {
+    if (activeRef.current) {
+      activeRef.current.scrollIntoView({
+        behavior: "smooth", // change to "auto" if you don't want animation
+        block: "center",
+      });
+    }
+  }, [location.pathname]);
 
   return (
     <div className="flex flex-col w-72 min-h-[calc(100vh-4rem)] h-full border-l bg-gray-50">
@@ -32,36 +43,44 @@ const SubSidbar = ({
             const isActive =
               location.pathname.split("/")[2] === route.href.split("/")[1] ||
               location.pathname === route.href;
+
             return (
-              <Link
+              <NavLink
                 key={route.name}
                 to={route.href}
-                className={`flex justify-center flex-col gap-y-1 px-3 py-3 text-sm font-medium rounded-md transition-all duration-200 shadow-[0_2px_10px_rgba(0,0,0,0.10)] ${
-                  isActive
-                    ? "bg-primary/30 text-primary"
-                    : "text-gray-700 hover:bg-gray-200 hover:text-gray-700"
-                } ${isLast ? "mb-2" : ""}`}
+                ref={isActive ? activeRef : null}
+                className={`flex flex-col gap-y-1 px-3 py-3 text-sm font-medium rounded-md transition-all duration-200 shadow-[0_2px_10px_rgba(0,0,0,0.10)] ${
+                    isActive
+                      ? "bg-primary/30 text-primary"
+                      : "text-gray-700 hover:bg-gray-200 hover:text-gray-700"
+                  } ${isLast ? "mb-2" : ""}`
+                }
               >
-                <div className="flex items-center">
-                  {route.icon && (
-                    <route.icon
-                      className={`mr-2 h-5 w-5 ${
-                        isActive ? "text-primary" : "text-gray-400"
-                      }`}
-                    />
-                  )}
-                  <div className="font-medium">{route?.name}</div>
-                </div>
-                {route?.sideDescription && (
-                  <div
-                    className={`text-xs ${
-                      isActive ? "text-primary/70" : "text-gray-400"
-                    }`}
-                  >
-                    {route?.sideDescription}
-                  </div>
+                {({ isActive }) => (
+                  <>
+                    <div className="flex items-center">
+                      {route.icon && (
+                        <route.icon
+                          className={`mr-2 h-5 w-5 ${
+                            isActive ? "text-primary" : "text-gray-400"
+                          }`}
+                        />
+                      )}
+                      <div className="font-medium">{route?.name}</div>
+                    </div>
+
+                    {route.sideDescription && (
+                      <div
+                        className={`text-xs ${
+                          isActive ? "text-primary/70" : "text-gray-400"
+                        }`}
+                      >
+                        {route.sideDescription}
+                      </div>
+                    )}
+                  </>
                 )}
-              </Link>
+              </NavLink>
             );
           })}
       </nav>
